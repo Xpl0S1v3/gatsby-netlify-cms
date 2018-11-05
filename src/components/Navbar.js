71420f8fdd52514ab33521493530fdabb0e492c0
Xpl0S1v3 <jochenraymaekers@gmail.com>
2018-11-05 16:24:44 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/Xpl0S1v3/gatsby-netlify-cms"
           target="_blank"
           rel="noopener noreferrer"
         >
